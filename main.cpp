56dd6ab8b20bed7e148b5e363cf0302b0925e332
Yoav <yoavnahum2005@gmail.com>
2022-07-24 00:18:38 +0300

    started main file

--- main.cpp
+++ main.cpp
@@ -1,2 +1,20 @@
-main() {
+#include "fileHandler.hpp"
+#include "iris.hpp"
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Converts a line into an Iris object.
+ * @param line line to convert
+ * @return Iris object
+ */
+Iris decryptToIris(string line) {
+    vector<string> fields = split(line, ',');
+    return Iris(stod(fields[0]), stod(fields[1]), stod(fields[2]), stod(fields[3]), Iris::getIrisType(fields[4]));
+}
+
+int main(int argc, char const *argv[]) {
+    vector<Iris> irisData = decryptFile<Iris>("iris.csv", decryptToIris);
+    return 0;
 }
