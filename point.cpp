4abbe95dca48396a2a9844db4b3fe97bcea4a203
Yoav <yoavnahum2005@gmail.com>
2022-07-24 00:15:57 +0300

    Converted to vector from int*

--- point.cpp
+++ point.cpp
@@ -1,29 +1,40 @@
 #include <math.h>
+#include <vector>
+
+using namespace std;
+
 class Point {
-    private:
-    double* fields;
-    int fieldsLength;
+
+    protected:
+    vector<double> fields;
+    int size;
+
     public:
+
+    Point(vector<double> fields) {
+        this->fields = fields;
+        size = fields.size();
+    }
+    
     double getEuclideanDistance(Point other) {
         double distance = 0;
-        for (int i = 0; i < fieldsLength; i++)
-        {
+        for (int i = 0; i < size; i++) {
             distance += pow(fields[i] - other.fields[i], 2);
         }
         return sqrt(distance);
     }
+
     double getManhattanDistance(Point other) {
         double distance = 0;
-        for (int i = 0; i < fieldsLength; i++)
-        {
+        for (int i = 0; i < size; i++) {
             distance += abs(fields[i] - other.fields[i]);
         }
         return distance;
     }
+
     double getChebyshevDistance(Point other) {
         double max = 0;
-        for (int i = 0; i < fieldsLength; i++)
-        {
+        for (int i = 0; i < size; i++) {
             if (abs(fields[i] - other.fields[i]) > max) {
                 max =  abs(fields[i] - other.fields[i]);
             }
---
+++ point.hpp
@@ -0,0 +1,36 @@
+class Point {
+
+    protected:
+    double* fields;
+    int fieldsLength;
+
+    public:
+
+    /**
+     * Constructor.
+     * @param fields vector of fields
+     */
+
+    Point(vector<double> fields); 
+
+    /**
+     * Get Euclidean distance between this point and other point.
+     * @param other other point
+     * @return Euclidean distance
+     */
+    double getEuclideanDistance(Point other);
+    
+    /**
+     * Get Manhattan distance between this point and other point.
+     * @param other other point
+     * @return Manhattan distance
+     */
+    double getManhattanDistance(Point other);
+
+    /**
+     * Get Chebyshev distance between this point and other point.
+     * @param other other point
+     * @return Chebyshev distance
+     */
+    double getChebyshevDistance(Point other);
+};
