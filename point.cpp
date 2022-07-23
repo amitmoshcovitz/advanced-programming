b4ec760b602fa3d743f47db72e65862e49583068
amitmoshcovitz <amitmos2005@gmail.com>
2022-07-23 12:32:36 -0700

    Added point distance

--- point.cpp
+++ point.cpp
@@ -1,14 +1,33 @@
+#include <math.h>
 class Point {
     private:
     double* fields;
     int fieldsLength;
     public:
-    double getPowerDistance(Point other) {
+    double getEuclideanDistance(Point other) {
         double distance = 0;
         for (int i = 0; i < fieldsLength; i++)
         {
-            distance += fields[i] * fields[i];
+            distance += pow(fields[i] - other.fields[i], 2);
         }
-        
+        return sqrt(distance);
+    }
+    double getManhattanDistance(Point other) {
+        double distance = 0;
+        for (int i = 0; i < fieldsLength; i++)
+        {
+            distance += abs(fields[i] - other.fields[i]);
+        }
+        return distance;
+    }
+    double getChebyshevDistance(Point other) {
+        double max = 0;
+        for (int i = 0; i < fieldsLength; i++)
+        {
+            if (abs(fields[i] - other.fields[i]) > max) {
+                max =  abs(fields[i] - other.fields[i]);
+            }
+        }
+        return max;
     }
 };
